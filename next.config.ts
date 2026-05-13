c1cf8be610f5a912470cfc6ac7719f25b0738467
Kido_Akihiro <a.k.recollectionex@gmail.com>
2026-05-13 18:40:41 +0900

    pnpmに置き換え

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config, { webpack }) => {
+    config.plugins.push(
+      new webpack.NormalModuleReplacementPlugin(/^node:/, (resource: { request: string }) => {
+        resource.request = resource.request.replace(/^node:/, "");
+      })
+    );
+    return config;
+  },
 };
 
 export default nextConfig;
